d1a6c3f1324bf4796331170dbe4872c52770facc
unknown <md.monirulmunna.cse@gmail.com>
2024-10-28 11:49:17 +0600

    changes in wsgi file

--- research_lab/wsgi.py
+++ research_lab/wsgi.py
@@ -7,10 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research_lab.settings')
+
+# application = get_wsgi_application()
+
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'research_lab.settings')
-
 application = get_wsgi_application()
+
+# Add this alias for Vercel
+app = application
