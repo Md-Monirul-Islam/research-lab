1ded6638f29c06536e9637b58dc3ca55da9f0c33
unknown <md.monirulmunna.cse@gmail.com>
2024-10-28 12:58:19 +0600

    chagnes in settings file

--- research_lab/settings.py
+++ research_lab/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-#railway connfigurations
+#railway
 STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files settings
